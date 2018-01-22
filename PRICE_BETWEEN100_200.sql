5d77e41b6077f38eb598e97be9f64c527a6f74ea
Jason Thatcher <contact@jasonthatcher.com>
2018-01-22 11:36:13 -0600

    alt between'

--- PRICE_BETWEEN100_200.sql
+++ PRICE_BETWEEN100_200.sql
@@ -1,10 +1,19 @@
 --Connect to Database
 USE WideWorldImporters;
 
---Select StockItem with value between $100 and $200
+--Select StockItem with value between $100 and $200 using BETWEEN operator
 SELECT	A.StockItemName
 ,		A.Brand
 ,		A.UnitPrice
 ,		A.Barcode
 FROM Warehouse.StockItems AS A
 WHERE A.UnitPrice BETWEEN 100 AND 200;
+
+--Select StockItem with value between $100 and $200 using between <, =, > operator
+SELECT	A.StockItemName
+,		A.Brand
+,		A.UnitPrice
+,		A.Barcode
+FROM Warehouse.StockItems AS A
+WHERE (A.UnitPrice >= 100
+		AND A.UnitPrice <= 200);
